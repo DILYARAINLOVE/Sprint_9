87928ee8c06772d0a4fbab799e1768381b0dd0cd
Dilyara Runova <dilyararunova@yandex.ru>
2026-02-26 10:52:30 +0300

    fix: registration test now uses name selectors and includes username

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,18 +1,46 @@
-# Sprint 9 review
+import sys
+import time
 from selenium.webdriver.common.by import By
-from .base_page import BasePage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
 
 class RegistrationPage(BasePage):
-    # Локаторы
-    _first_name_input = (By.NAME, "firstName")   # уточните по реальному приложению
-    _last_name_input = (By.NAME, "lastName")
+    # Используем атрибуты name (самые надёжные)
+    _first_name_input = (By.NAME, "first_name")
+    _last_name_input = (By.NAME, "last_name")
+    _username_input = (By.NAME, "username")
     _email_input = (By.NAME, "email")
     _password_input = (By.NAME, "password")
-    _register_button = (By.XPATH, "//button[contains(text(),'Создать аккаунт')]")
+    _submit_button = (By.XPATH, "//button[text()='Создать аккаунт']")
 
     def register(self, first_name, last_name, email, password):
+        print(f"\n>>> Регистрация: заполняем first_name")
         self.send_keys(self._first_name_input, first_name)
+        print(">>> first_name заполнено")
+
         self.send_keys(self._last_name_input, last_name)
+        print(">>> last_name заполнено")
+
+        # Генерируем username из email (часть до @)
+        username = email.split('@')[0]
+        print(f">>> Заполняем username: {username}")
+        self.send_keys(self._username_input, username)
+        print(">>> username заполнено")
+
         self.send_keys(self._email_input, email)
+        print(">>> email заполнено")
+
         self.send_keys(self._password_input, password)
-        self.click(self._register_button)
+        print(">>> password заполнено")
+        sys.stdout.flush()
+
+        # Даём время JS активировать кнопку
+        time.sleep(1)
+
+        print(">>> Ожидаем активацию кнопки отправки")
+        button = self.wait.until(EC.element_to_be_clickable(self._submit_button))
+        print(">>> Кнопка активна, кликаем")
+        button.click()
+        print(">>> Форма отправлена")
+        sys.stdout.flush()
